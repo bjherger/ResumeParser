97d01e6f0ad51d78bd978603863a266ffb538e2c
Brendan J. Herger <b@revup.com>
2016-10-22 17:24:06 -0700

    refactoring of create_resume_df

--- bin/ResumeChecker.py
+++ bin/ResumeChecker.py
@@ -7,6 +7,7 @@ custom text fields. These results are then surfaced as a convenient summary CSV.
 
 """
 import argparse
+import csv
 import functools
 import glob
 import logging
@@ -33,13 +34,13 @@ __maintainer__ = 'bjherger'
 
 def main():
     """
-    Main method for ResumeParser. This utility will read the data_path and output path.
-
-    With this information, this utility will then:
+    Main method for ResumeParser. This utility will:
+     - Read in `data_path` and `output_path` from command line arguments
      - Create a list of documents to scan
      - Read the text from those documents
      - Pull out desired information (e.g. contact info, skills, custom text fields)
      - Output summary CSV
+
     :return: None
     :rtype: None
     """
@@ -63,17 +64,8 @@ def main():
     resume_df = create_resume_df(args.data_path)
 
     # Output to CSV
-    resume_df.to_csv(args.output_path)
+    resume_df.to_csv(args.output_path, quoting=csv.QUOTE_ALL, encoding='utf-8')
     logging.info('End Main')
-
-
-def list_files(path=os.getcwd()):
-    file_list = list()
-    for (dirpath, dirnames, filenames) in os.walk(path):
-        os.path.join(dirpath)
-        filenames = [os.path.join(dirpath, filename) for filename in filenames]
-        file_list.extend(filenames)
-    return file_list
 
 
 def convert_pdf_to_txt(path):
@@ -124,8 +116,8 @@ def check_phonenumber(string_to_search):
                                         r"[\s\.-]{0,2}?"  # area code, phone separator
                                         r"(\d{3})"  # 3 digit local
                                         r"[\s\.-]{0,2}"  # 3 digit local, 4 digit local separator
-                                        r"(\d{4})"  # 4 digit local
-                                        , re.IGNORECASE)
+                                        r"(\d{4})",  # 4 digit local
+                                        re.IGNORECASE)
         result = re.search(regular_expression, string_to_search)
         if result:
             result = result.groups()
@@ -138,7 +130,6 @@ def check_phonenumber(string_to_search):
 
 def check_email(string_to_search):
     try:
-        # regular_expression = re.compile(r"(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})")
         regular_expression = re.compile(r"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}", re.IGNORECASE)
         result = re.search(regular_expression, string_to_search)
         if result:
@@ -151,7 +142,6 @@ def check_email(string_to_search):
 
 def check_address(string_to_search):
     try:
-        # regular_expression = re.compile(r"(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})")
         regular_expression = re.compile(r"[0-9]+ [a-z0-9,\.# ]+\bCA\b", re.IGNORECASE)
         result = re.search(regular_expression, string_to_search)
         if result:
@@ -186,47 +176,67 @@ def term_match(string_to_search, term="me"):
         return None
 
 
-def create_resume_df(path):
-    logging.info('Searching path: ' + str(path))
-    path_glob = os.path.join(path, '*.pdf')
-    file_list = [pdf_file for pdf_file in glob.glob(path_glob)]
+def create_resume_df(data_path):
+    """
+
+    This function creates a Pandas DF with one row for every input resume, and columns including the resumes's
+    file path and raw text.
+
+    This is achieved through the following steps:
+     - Create a list of documents to scan
+     - Read the text from those documents
+     - Pull out desired information (e.g. contact info, skills, custom text fields)
+    :param data_path: Path to a folder containing resumes. Any files ending in .pdf in this folder will be treated as a
+    resume.
+    :type data_path: str
+    :return: A Pandas DF with one row for every input resume, and columns including the resumes's
+    file path and raw text
+    :rtype: pd.DataFrame
+    """
+
+    # Create a list of documents to scan
+    logging.info('Searching path: ' + str(data_path))
+
+    # Find all files in the data_path which end in `.pdf`. These will all be treated as resumes
+    path_glob = os.path.join(data_path, '*.pdf')
+
+    # Create list of files
+    file_list = glob.glob(path_glob)
 
     logging.info('Iterating through file_list: ' + str(file_list))
     df = pd.DataFrame()
 
-    # Get file information
-    df["file_name"] = file_list
-    df["html"] = df["file_name"].apply(convert_pdf_to_txt)
+    # Store metadata, raw text, and word count
+    df["file_path"] = file_list
+    df["raw_text"] = df["file_path"].apply(convert_pdf_to_txt)
+    df["num_words"] = df["raw_text"].apply(lambda x: len(x.split()))
 
     # Scrape contact information
-    df["phone_number"] = df["html"].apply(check_phonenumber)
+    df["phone_number"] = df["raw_text"].apply(check_phonenumber)
     df["area_code"] = df["phone_number"].apply(functools.partial(term_match, term=r"\d{3}"))
-    df["email"] = df["html"].apply(check_email)
+    df["email"] = df["raw_text"].apply(check_email)
     df["email_domain"] = df["email"].apply(functools.partial(term_match, term=r"@(.+)"))
-    df["address"] = df["html"].apply(check_address)
-    df["linkedin"] = df["html"].apply(functools.partial(term_count, term=r"linkedin"))
-    df["github"] = df["html"].apply(functools.partial(term_count, term=r"github"))
+    df["address"] = df["raw_text"].apply(check_address)
+    df["linkedin"] = df["raw_text"].apply(functools.partial(term_count, term=r"linkedin"))
+    df["github"] = df["raw_text"].apply(functools.partial(term_count, term=r"github"))
 
-    # Pull a few other interesting features
-    df["num_words"] = df["html"].apply(lambda x: len(x.split()))
-    df["phd"] = df["html"].apply(functools.partial(term_count, term=r"ph.?d.?"))
+    # Scrape education information
+    df["phd"] = df["raw_text"].apply(functools.partial(term_count, term=r"ph.?d.?"))
 
-    # And skills
-    df["java_count"] = df["html"].apply(functools.partial(term_count, term=r"java"))
-    df["python_count"] = df["html"].apply(functools.partial(term_count, term=r"python"))
-    df["R_count"] = df["html"].apply(functools.partial(term_count, term=r" R[ ,]"))
-    df["latex_count"] = df["html"].apply(functools.partial(term_count, term=r"latex"))
-    df["stata_count"] = df["html"].apply(functools.partial(term_count, term=r"stata"))
-    df["CS_count"] = df["html"].apply(functools.partial(term_count, term=r"computer science"))
-    df["mysql_count"] = df["html"].apply(functools.partial(term_count, term=r"mysql"))
-    df["ms_office"] = df["html"].apply(functools.partial(term_count, term=r"microsoft office"))
-    df["analytics"] = df["html"].apply(functools.partial(term_count, term=r"analytics"))
+    # Scrape skill information
+    df["java_count"] = df["raw_text"].apply(functools.partial(term_count, term=r"java"))
+    df["python_count"] = df["raw_text"].apply(functools.partial(term_count, term=r"python"))
+    df["R_count"] = df["raw_text"].apply(functools.partial(term_count, term=r" R[ ,]"))
+    df["latex_count"] = df["raw_text"].apply(functools.partial(term_count, term=r"latex"))
+    df["stata_count"] = df["raw_text"].apply(functools.partial(term_count, term=r"stata"))
+    df["CS_count"] = df["raw_text"].apply(functools.partial(term_count, term=r"computer science"))
+    df["mysql_count"] = df["raw_text"].apply(functools.partial(term_count, term=r"mysql"))
+    df["ms_office"] = df["raw_text"].apply(functools.partial(term_count, term=r"microsoft office"))
+    df["analytics"] = df["raw_text"].apply(functools.partial(term_count, term=r"analytics"))
 
+    # Return enriched DF
     return df
 
-
-# main
-# *********************************
 
 if __name__ == '__main__':
     main()
