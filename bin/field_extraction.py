a5357a91e141ec6ed0be0dc92ac6009b9e22e778
Saad A. Malik <saadmalik95@gmail.com>
2020-07-27 23:20:39 -0400

    Update field_extraction.py

--- bin/field_extraction.py
+++ bin/field_extraction.py
@@ -6,6 +6,7 @@ from bin import lib
 
 EMAIL_REGEX = r"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}"
 PHONE_REGEX = r"\(?(\d{3})?\)?[\s\.-]{0,2}?(\d{3})[\s\.-]{0,2}(\d{4})"
+NAME_REGEX = r'[a-z]+(\s+[a-z]+)?'
 
 
 def candidate_name_extractor(input_string, nlp):
